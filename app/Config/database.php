d92c0682efc5da8131253c0fa97c0e1036249644
gjunior <gjunior@commerce.dev.tray.intranet>
2013-05-06 18:06:05 -0300

    Subindo restante dos arquivos

---
+++ app/Config/database.php
@@ -0,0 +1,12 @@
+<?php
+class DATABASE_CONFIG {
+
+	public $default = array(
+		'datasource' => 'Database/Mysql',
+		'persistent' => false,
+		'host' => 'localhost',
+		'login' => 'tray',
+		'password' => 'tray77',
+		'database' => 'xeon_310010_gjunior',
+	);
+}
